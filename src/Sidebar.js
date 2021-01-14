2f1aff8d7b213f68bf5cb55c9197afdbe62f2ce5
Your Name <chris.annor@me.com>
2021-01-14 14:54:36 +0000

    Styling to icons in header left

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,13 @@ import "./Sidebar.css";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div class="sidebar__header">
-        <div class="sidebar__headerLeft"></div>
+        <Avatar src="" />
         <div class="sidebar__headerRight">
           <IconButton>
             <ChatIcon />
