fbd69713111e02d8fd625e5bd96b74a86b10279c
Your Name <chris.annor@me.com>
2021-01-14 14:38:32 +0000

    Add clickable icons to the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton } from "@material-ui/core";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <h1>Sidebar</h1>
+  return (
+    <div className="sidebar">
+      <div class="sidebar__header">
+        <div class="sidebar__headerLeft"></div>
+        <div class="sidebar__headerRight">
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
-
-
+export default Sidebar;
