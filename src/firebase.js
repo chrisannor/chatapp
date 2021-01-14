f8d5169538a86737ce80e485e0eeca2146d153b6
chrisannor <chris.annor@me.com>
2021-01-14 13:37:10 +0000

    Initialised app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBA511iOrJ9YFlVyntTxhDxli0A16Fa4zg",
+    authDomain: "chatapp-chocolate.firebaseapp.com",
+    projectId: "chatapp-chocolate",
+    storageBucket: "chatapp-chocolate.appspot.com",
+    messagingSenderId: "198505243508",
+    appId: "1:198505243508:web:2f22c253251b56b5fc4221",
+    measurementId: "G-6MZ5S42THF"
+  };
