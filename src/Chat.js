24323fd726ddc7a745d465afdbbbc5ebb576e5f5
Your Name <chris.annor@me.com>
2021-01-14 15:50:09 +0000

    Add current chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,31 @@
-
-import React from 'react'
-import "./Chat.css"
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./Chat.css";
 
 function Chat() {
-    return (
-        <div className="chat">
-            <h1>Chat</h1>
+  return (
+    <div className="chat">
+      <div class="chat__header">
+        <Avatar />
+        <div class="chat__headerInfo">
+          <h3>Contact Name</h3>
+          <p>Last seen at...</p>
         </div>
-    )
+        <div class="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
